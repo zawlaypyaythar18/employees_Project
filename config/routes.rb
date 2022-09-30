a86bb66962ad88d06ba4ae261ebc2c83c8ec61b4
Zaw Lay Pyay Thar <zawlaypyaythar2@gmail.com>
2022-09-30 10:26:01 +0630

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # get 'home/index'
+
+  root 'home#index'
+  get 'home/addemployee'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
